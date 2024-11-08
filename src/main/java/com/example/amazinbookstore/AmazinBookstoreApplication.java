4a28ea22eab0b22582e508db45db24acdcb89ac7
Tuna Uygun <uyguntuna2018@gmail.com>
2024-11-08 17:20:27 -0500

    Initial commit

---
+++ src/main/java/com/example/amazinbookstore/AmazinBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazinbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amazinbookstore/AmazinBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazinbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazinBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
