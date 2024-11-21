e118676005489c29c60a12c02bf0465f20033303
Tuna Uygun <uyguntuna2018@gmail.com>
2024-11-20 21:06:40 -0500

    Updated cart controllers to add item to cart

--- src/main/java/com/example/amazinbookstore/controllers/CartController.java
+++ src/main/java/com/example/amazinbookstore/controllers/CartController.java
@@ -1,18 +1,45 @@
 package com.example.amazinbookstore.controllers;
 
+import com.example.amazinbookstore.entities.Book;
+import com.example.amazinbookstore.entities.Cart;
+import com.example.amazinbookstore.entities.CartItem;
+import com.example.amazinbookstore.repositories.BookRepository;
+import com.example.amazinbookstore.repositories.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
 
-    @PostMapping
-    public RedirectView addNewBookToCart(@RequestParam("bookId") String bookId) {
-        // We will add the code to add the book to the cart in the next milestone
+    @Autowired
+    private CartRepository cartRepository;
 
-        // For this milestone, we just print the book id
-        System.out.println("Book ID to be added to cart: " + bookId);
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostMapping
+    public RedirectView addNewBookToCart(@RequestParam("bookId") Long bookId) {
+
+        // TODO:
+        // As the user login functionality is under development, we just get the first cart,
+        // or create a new one if there is none.
+        Optional<Cart> cartOptional = cartRepository.findById(1L);
+        Cart cart = cartOptional.orElseGet(Cart::new);
+
+        Optional<Book> book = bookRepository.findById(bookId);
+        if(book.isPresent()) {
+            CartItem cartItem = new CartItem();
+            cartItem.setBook(book.get());
+            cartItem.setQuantity(1);
+            cart.addCartItem(cartItem);
+            cartRepository.save(cart);
+            System.out.println("Book ID to be added to cart: " + bookId);
+        }
+
         return new RedirectView("/amazinBookstore/cart");
     }
 }
--- src/main/java/com/example/amazinbookstore/controllers/CartViewController.java
+++ src/main/java/com/example/amazinbookstore/controllers/CartViewController.java
@@ -1,7 +1,10 @@
 package com.example.amazinbookstore.controllers;
 
 import com.example.amazinbookstore.entities.Book;
+import com.example.amazinbookstore.entities.Cart;
+import com.example.amazinbookstore.entities.CartItem;
 import com.example.amazinbookstore.repositories.BookRepository;
+import com.example.amazinbookstore.repositories.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/amazinBookstore")
@@ -17,10 +21,19 @@ public class CartViewController {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private CartRepository cartRepository;
+
+    // TODO:
+    // As the user login functionality is under development, we just get the first cart,
+    // or create a new one if there is none.
     @GetMapping("/cart")
     public String viewCartPage(Model model) {
-        List<Book> books = bookRepository.findAll();
-        model.addAttribute("books", books);
+        Optional<Cart> cartOptional = cartRepository.findById(1L);
+        Cart cart = cartOptional.orElseGet(Cart::new);
+        List<CartItem> cartItems = cart.getCartItems();
+
+        model.addAttribute("cartItems", cartItems);
         return "Cart_Page";
     }
 }
