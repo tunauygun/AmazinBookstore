d0e398f61244e47260249854915e7187db5da0b2
mikaa <mika.argyle@gmail.com>
2024-11-22 14:55:21 -0500

    Added User and UserRepository classes (refactored to match current Cart/Book and their respective repositories)

--- src/main/java/com/example/amazinbookstore/entities/User.java
+++ src/main/java/com/example/amazinbookstore/entities/User.java
@@ -6,9 +6,7 @@ import jakarta.persistence.*;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Boolean isAdmin;
